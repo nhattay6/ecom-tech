629287986d222511ba97244b5eba0d7c782e184c
DESKTOP-AV96EGN\Admin <tayhaithanh68@gmail.com>
2024-01-03 23:03:06 +0700

    success response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,15 +1,20 @@
 'use strict'
 
 const AccessService = require("../services/access.service")
+const {OK, CREATED} = require('../core/success.response')
 class AccessController {
   signUp = async (req, res, next) => {
-    // try {
-      return res.status(201).json(
-        await AccessService.signUp(req.body)
-      )
-    // } catch (error) {
-    //   next(error)
-    // }
+    // return res.status(200).json({
+    //   message: '',
+    //   metadata: '',
+    // })
+    new CREATED({
+      message: 'Registed OK!',
+      metadata: await AccessService.signUp(req.body),
+      options: {
+        limit: 10
+      }
+    }).send(res)
   } 
 }
 
